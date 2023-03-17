64296970daea6a1b0eb00fbcd90fdb4d0ba3817a
mhdshabeebvp <sshabeeb33@gmail.com>
2023-03-17 23:23:36 +0530

    Intro section started, a small modification happend in the Navbar.css

--- src/Component/Intro/Intro.js
+++ src/Component/Intro/Intro.js
@@ -1,22 +1,20 @@
 import React from "react";
-
+import "./Intro.css";
 const Intro = () => {
   return (
-    <div>
-      <div className="intro">
-        <div className="i-left">
-          <div className="i-name">
-            <span>Hy! I Am </span>
-            <span>Muhammd Shabeeb</span>
-            <span>
-              Frontend Developer with high level of experience in web
-              design,producing the Quality works
-            </span>
-          </div>
-          <button className="">Hire Me</button>i-button
+    <div className="intro">
+      <div className="i-left">
+        <div className="i-name">
+          <span>Hy! I Am </span>
+          <span>Muhammd Shabeeb</span>
+          <span>
+            Frontend Developer with high level of experience in web
+            design,producing the Quality works
+          </span>
         </div>
-        <div className="i-right"></div>
+        <button className="button i-button">Hire Me</button>
       </div>
+      <div className="i-right"> i am right side</div>
     </div>
   );
 };
