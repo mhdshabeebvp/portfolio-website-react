b279fa5b5a4dc6285f1380cdf5f84480fc0cfe61
mhdshabeebvp <sshabeeb33@gmail.com>
2023-03-17 16:45:29 +0530

    Intro-section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Navbar from "./Component/Navbar/Navbar";
 import "./App.css"
+import Intro from "./Component/Intro/Intro";
+import Navbar from "./Component/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
       <Navbar />
+     <Intro />
     </div>
   );
 }
---
+++ src/Component/Intro/Intro.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div>
+      <div className="intro">
+        <div className="i-left">
+          <div className="i-name">
+            <span>Hy! I Am </span>
+            <span>Muhammd Shabeeb</span>
+            <span>
+              Frontend Developer with high level of experience in web
+              design,producing the Quality works
+            </span>
+          </div>
+          <button className="">Hire Me</button>i-button
+        </div>
+        <div className="i-right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
