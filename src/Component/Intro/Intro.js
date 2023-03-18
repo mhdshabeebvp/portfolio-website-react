d5af387d71b55db5fb6417075a03130600088d46
mhdshabeebvp <sshabeeb33@gmail.com>
2023-03-18 13:33:37 +0530

    INTRO-SECTION ==> add the css of social media and alos the right section started with css

--- src/Component/Intro/Intro.js
+++ src/Component/Intro/Intro.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Intro.css";
+// Icons section started
 import Github from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
 import Youtube from "../../img/youtube.png";
-
+// icons section ended
+// RIGHT SECTION IMAGES
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import Boy from "../../img/boy.png";
+import Thumbup from "../../img/thumbup.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import Crown from "../../img/crown.png";
 const Intro = () => {
   return (
     <div className="intro">
@@ -22,21 +30,40 @@ const Intro = () => {
           <a
             href="https://github.com/mhdshabeebvp/portfolio-website-react/commits/master"
             target={"_blank"}
+            rel="noreferrer"
           >
             <img src={Github} alt="" />
           </a>
-          <a href="https://www.linkedin.com/feed/" target={"_blank"}>
+          <a
+            href="https://www.linkedin.com/feed/"
+            target={"_blank"}
+            rel="noreferrer"
+          >
             <img src={LinkedIn} alt="" />
           </a>
-          <a href="https://www.instagram.com/" target={"_blank"}>
+          <a
+            href="https://www.instagram.com/"
+            target={"_blank"}
+            rel="noreferrer"
+          >
             <img src={Instagram} alt="" />
           </a>
-          <a href="https://www.youtube.com/ " target={"_blank"}>
+          <a
+            href="https://www.youtube.com/ "
+            target={"_blank"}
+            rel="noreferrer"
+          >
             <img src={Youtube} alt="" />
           </a>
         </div>
       </div>
-      <div className="i-right"> i am right side</div>
+
+      {/* INTRO RIGHT SECTION */}
+      <div className="i-right">
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={Boy} alt="" />
+      </div>
     </div>
   );
 };
