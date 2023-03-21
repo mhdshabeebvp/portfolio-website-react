90d19d20d49a97522bc1bd20653ffca9c191e412
mhdshabeebvp <sshabeeb33@gmail.com>
2023-03-21 12:06:54 +0530

    footer section completed.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Experience from "./Component/Experience/Experience";
+import Footer from "./Component/footer/Footer";
 import Intro from "./Component/Intro/Intro";
 import Navbar from "./Component/Navbar/Navbar";
 import Services from "./Component/Services/Services";
@@ -8,11 +9,12 @@ import Testimonial from "./Component/Testimonial/Testimonial";
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar /> 
       <Intro />
       <Services />
       <Experience />
       <Testimonial />
+      <Footer />
     </div>
   );
 }
