095ab0e4890554e135ea83f2a7cb446d8693f3c6
mhdshabeebvp <sshabeeb33@gmail.com>
2023-03-27 15:06:01 +0530

    some changes are updated

--- src/Component/Intro/Intro.js
+++ src/Component/Intro/Intro.js
@@ -7,17 +7,17 @@ import Instagram from "../../img/instagram.png";
 import Youtube from "../../img/youtube.png";
 // icons section ended
 // RIGHT SECTION IMAGES
-import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-import Boy from "../../img/boy.png";
-// import Newboy from "../../img/NEWBOY.png"
+import Newboy from "../../img/NEWBOY.png";
 import Thumbup from "../../img/thumbup.png";
+import reactIcon from "../../img/react.svg";
 import glassesimoji from "../../img/glassesimoji.png";
 import Crown from "../../img/crown.png";
 import FloatingDiv from "../FloatingDiv/FloatingDiv";
 import { themeContext } from "../../Context";
 import { useContext } from "react";
 import { motion } from "framer-motion";
+import Resume from "../../Component/Intro/Resume.pdf"
 
 const Intro = () => {
   const transition = { duration: 2, type: "spring" };
@@ -41,7 +41,9 @@ const Intro = () => {
             ever-evolving field.
           </span>
         </div>
-        <button className="button i-button">Hire Me</button>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
         {/* social icon */}
         <div className="i-icons">
           <a
@@ -73,9 +75,9 @@ const Intro = () => {
 
       {/* INTRO RIGHT SECTION */}
       <div className="i-right">
-        <img src={Vector1} alt="" />
+        <img src={reactIcon} alt="" />
         <img src={Vector2} alt="" />
-        <img src={Boy} alt="" />
+        <img src={Newboy} alt="" />
         {/* motion animation */}
         <motion.img
           initial={{ left: "-36%" }}
