481f8b80bdeb1ed14714f67b6841f5a65bba8ddd
mhdshabeebvp <sshabeeb33@gmail.com>
2023-04-06 06:25:19 +0530

    test only

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,66 @@
+import React, { useRef, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTwitter, faInstagram, faGithub, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import "./Footer.css";
+
+const Footer = () => {
+  const footerRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          footerRef.current.classList.add("animate");
+        } else {
+          footerRef.current.classList.remove("animate");
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+    observer.observe(footerRef.current);
+    return () => observer.unobserve(footerRef.current);
+  }, []);
+
+  return (
+    <footer className="footer" ref={footerRef}>
+      <div className="left-section">
+        <h2 className="footer-heading">Let's make something </h2>
+        <p className="footer-text">amazing together.</p>
+      </div>
+      <div className="right-section">
+        <ul className="social-links">
+          <li className="social-link">
+            <a href="#">
+              <FontAwesomeIcon icon={faFacebook} className="social-icon" />
+            </a>
+          </li>
+          <li className="social-link">
+            <a href="#">
+              <FontAwesomeIcon icon={faTwitter} className="social-icon" />
+            </a>
+          </li>
+          <li className="social-link">
+            <a href="#">
+              <FontAwesomeIcon icon={faInstagram} className="social-icon" />
+            </a>
+          </li>
+          <li className="social-link">
+            <a href="#">
+              <FontAwesomeIcon icon={faGithub} className="social-icon" />
+            </a>
+          </li>
+          <li className="social-link">
+            <a href="#">
+              <FontAwesomeIcon icon={faYoutube} className="social-icon" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Component/Intro/sample.js
@@ -0,0 +1,165 @@
+import React, { Component } from "react";
+import TagCanvas from 'tag-canvas';
+import "./Intro.css";
+// Icons section started
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
+import Youtube from "../../img/youtube.png";
+// icons section ended
+// RIGHT SECTION IMAGES
+import { themeContext } from "../../Context";
+import { useContext } from "react";
+import Resume from "../../Component/Intro/Resume.pdf";
+const Intro = () => {
+
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // // Right side heading animation
+  // const headings = ["Hardware", "Embedded", "Good", "Nice"];
+  // const [index, setIndex] = useState(0);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setIndex((prevIndex) => (prevIndex + 1) % headings.length);
+  //   }, 2000);
+  //   return () => clearInterval(interval);
+  // }, []);
+  constructor(props) {
+    super(props);
+    this.state = {
+      skills: [
+        { name: 'HTML', url: '#' },
+        { name: 'CSS', url: '#' },
+        { name: 'ES5', url: '#' },
+        { name: 'TypeScript', url: '#' },
+        { name: 'REST', url: '#' },
+        { name: 'JSON', url: '#' },
+        { name: 'GSAP', url: '#' },
+        { name: 'Data Science', url: '#' },
+        { name: 'Wordpress', url: '#' },
+        { name: 'PHP', url: '#' },
+        { name: 'Python', url: '#' },
+        { name: 'Node JS', url: '#' },
+        { name: 'Git', url: '#' },
+        { name: '_lodash', url: '#' },
+        { name: 'SASS', url: '#' },
+        { name: 'JQuery', url: '#' },
+        { name: 'SQI', url: '#' },
+        { name: 'Gulp', url: '#' },
+        { name: 'npm', url: '#' },
+        { name: 'BEM', url: '#' },
+      ],
+    };
+    
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const { skills } = this.state;
+    const options = {
+      textColour: '#08fdd8',
+      outlineColour: 'transparent',
+      reverse: true,
+      depth: 0.8,
+      maxSpeed: 0.05,
+      weight: true,
+    };
+    const canvas = this.canvasRef.current;
+    if (canvas && typeof TagCanvas !== 'undefined') {
+      TagCanvas.Start('myCanvas', 'tags', options);
+    }
+  }
+  render() {
+    const { skills } = this.state;
+  return (
+    <div className="intro" id="Intro">
+      <div className="i-left">
+        <div className="i-name">
+          <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am </span>
+          <span>Muhammd Shabeeb</span>
+          <span>
+            With a strong passion for technology and programming. I am skilled
+            in several programming languages, including PYTHON, JAVASCRIPT,
+            HTML, CSS etc..and have used these skills to develop websites, web
+            applications, and projects in hardware, robotics, and IoT. I am
+            committed to learning and exploring new ideas to stay up-to-date
+            with the latest trends and innovations in technology. I believe that
+            technology can be a powerful tool to solve complex problems and
+            improve people's lives, and I am excited to be a part of this
+            ever-evolving field.
+          </span>
+        </div>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        {/* social icon */}
+        <div className="i-icons">
+          <a
+            href="https://github.com/mhdshabeebvp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Github} alt="" rel="noreferrer" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/muhamed-shabeeb-vp-a24917229/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={LinkedIn} alt="" rel="noreferrer" />
+          </a>
+          <a
+            href="https://www.instagram.com/mhdshabeeb786"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Instagram} alt="" rel="noreferrer" />
+          </a>
+          <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+            <img src={Youtube} alt="" />
+          </a>
+        </div>
+      </div>
+
+      {/* INTRO RIGHT SECTION */}
+      <div className="i-right">
+      {/* <div className="TextHidden" style={{ animation: "fade-in 2s ease-in-out forwards" }}>
+      <h1>{headings[index]}</h1>
+    </div> */}
+
+    <div className="skills-charts">
+        <div id="myCanvasContainer">
+          <canvas width="500" height="500" id="myCanvas" ref={this.canvasRef}>
+            <ul id="tags">
+              {skills.map((skill, index) => (
+                <li key={index}>
+                  <a href={skill.url} target="_blank">
+                    {skill.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </canvas>
+        </div>
+      </div>
+
+        {/* blur div  */}
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div
+          className="blur"
+          style={{
+            background: "#C1F5FF",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
