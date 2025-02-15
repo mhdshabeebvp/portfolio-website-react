2786cfd294e750590cecdb6330d535370875a93d
mhdshabeebvp <sshabeeb33@gmail.com>
2025-02-15 12:36:53 +0530

    update the activities sections

--- src/Component/Activities/Activities.js
+++ src/Component/Activities/Activities.js
@@ -2,38 +2,61 @@ import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Scrollbars } from "react-custom-scrollbars-2";
 import "./Activities.css";
-import competitionImage from "../../img/activities/1689792545900.jpeg";
+import competitionImage from "../../img/activitiesimg/robotclass.jpeg";
+import yip from "../../img/activitiesimg/yip.jpeg";
+import printer from "../../img/activitiesimg/3d.jpg";
+import alhuda from "../../img/activitiesimg/alhuda.jpeg";
+import amup from "../../img/activitiesimg/amup.jpeg";
+import camp2 from "../../img/activitiesimg/camp2.jpeg";
+import cnc from "../../img/activitiesimg/cnc.jpeg";
+import farookstate from "../../img/activitiesimg/farookstate.jpeg";
+import malabar from "../../img/activitiesimg/malabar.jpg";
+
 const activities = [
     {
-        title: "Competitions",
-        description: " I had the privilege of attending a robotics class at the prestigious KNM English School in Ponnani. ",
+        title: "Robotics Class at KNM English School, Ponnani",
+        description: " I had the privilege of attending a robotics class at the prestigious KNM English School in PonnaniConducted a robotics class for talented 6th to 10th-grade students, sharing knowledge and inspiring their passion for technology. Their enthusiasm and grasp of robotics showcased their potential to become future innovators. ",
         imgSrc: competitionImage,
     },
     {
-        title: "Competitions",
-        description: " I had the privilege of attending a robotics class at the prestigious KNM English School in Ponnani. ",
-        imgSrc: competitionImage,
+        title: "Young Innovation Programme (YIP) – District Level Winner",
+        description: " Awarded a prize of ₹25,000 and selected for the state-level competition in July 2023. This prestigious program, organized by the Kerala Development and Innovation Strategic Council, recognizes innovative solutions and promotes creative problem-solving skills.",
+        imgSrc: yip,
     },
     {
-        title: "Competitions",
-        description: " I had the privilege of attending a robotics class at the prestigious KNM English School in Ponnani. ",
-        imgSrc: competitionImage,
-    },
-
-    {
-        title: "Competitions",
-        description: " I had the privilege of attending a robotics class at the prestigious KNM English School in Ponnani. ",
-        imgSrc: competitionImage,
+        title: "CNC 3D Printer from Electronic Waste",
+        description: " Developed a functional CNC 3D printer using repurposed electronic waste. This project highlights ingenuity and sustainability by transforming discarded materials into a versatile and efficient manufacturing tool.",
+        imgSrc: printer,
     },
     {
-        title: "Competitions",
-        description: " I had the privilege of attending a robotics class at the prestigious KNM English School in Ponnani. ",
-        imgSrc: competitionImage,
+        title: "Judge at TechFest – AL Huda Central School, Kadampuzha",
+        description: " Served as a judge for project evaluations at the TechFest hosted by AL Huda Central School, Kadampuzha. Assessed innovative student projects and provided feedback to inspire creativity and innovation. ",
+        imgSrc: alhuda,
     },
     {
-        title: "Competitions",
-        description: " I had the privilege of attending a robotics class at the prestigious KNM English School in Ponnani. ",
-        imgSrc: competitionImage,
+        title: "Robotics Class at AMUP School, Ullanam",
+        description: " Conducted a robotics class for students at AMUP School, Ullanam. Shared knowledge on robotics, encouraging creativity and fostering technical skills among young, enthusiastic learners.",
+        imgSrc: amup,
+    },
+    {
+        title: "Robotics Workshop at SKSBV Residential Camp",
+        description: "Conducted a robotics workshop at the SKSBV Residential Camp on May 16-17, 2024. Engaged students with hands-on learning, fostering technical skills and enthusiasm for robotics and innovation. ",
+        imgSrc: camp2,
+    },
+    {
+        title: "CNC Plotter Machine from Electronic Waste",
+        description: " Built a small CNC plotter machine using repurposed electronic waste. This project emphasizes creativity and sustainability by transforming discarded materials into a functional plotting device for precision drawing and design.",
+        imgSrc: cnc,
+    },
+    {
+        title: "Judge at TechFest – Farook English Medium School Kottakka",
+        description: "Served as a judge for project evaluations at the TechFest hosted by Farook English Medium School. Assessed innovative student projects, offering feedback to inspire creativity and technical excellence. ",
+        imgSrc: farookstate,
+    },
+    {
+        title: "Malabar Innovation Festival 2018 – Special Prize Winner",
+        description: " Participated in the Design & Challenge Contest at Malabar Innovation Festival 2018, hosted by the Innovation Hub (Regional Science Centre & Planetarium). Won a special prize in the Innovation Project category for a CNC machine project.",
+        imgSrc: malabar,
     },
 ];
 
